04298cdbedf33a63445be849dfa311eb02faed7d
Yurii-P-Biliai <u.p.bilyay@npu.edu.ua>
2020-04-15 22:06:46 +0300

    Task 02 from lab 02 done

---
+++ src/main/java/ua/edu/npu/lab02/task02/Application.java
@@ -0,0 +1,11 @@
+package ua.edu.npu.lab02.task02;
+
+public class Application {
+    public static void main(String[] args) {
+        int s = 0;
+        for (int i=10; i>0; i--) {
+            s+=i;
+            System.out.println(s);
+        }
+    }
+}
